e5611758b716b260084f7f28ecfb33ee10773432
Benjamin Clewer <benjiclewer@gmail.com>
2021-09-17 15:19:29 +0930

    progress, generate password functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,57 +1,45 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var length = Math.floor((Math.random() * 128 + 8)*Math.ceil(0.9926));
-var upper = true;
-var lower = true;
-var numbers = true;
-var specialCharacters = true;
+// default variables
+var lengthMax = 128;
+var lengthMin = 8;
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var number = "0123456789";
+var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var password = "";
 
-console.log(length);
 
-var passwordLength = function() {
-  var userLength = window.prompt("Select a password length between 8 and 128 characters.");
-  if (userLength <8) {
-    userLength = window.prompt("That number was less than 8, please select a number between 8 and 128 characters");
+function generatePassword() {
+  var p1 = "";
+  var lowerMessage = confirm("Do you want to include lower case characters in your password?");
+  if (lowerMessage){
+    p1 = lower;
+    console.log(p1);
+  } else {
+    p1 = "";
+    console.log(p1);
   }
-  else if (userLength >128) {
-    userLength = window.prompt("That number was greater than 128, please select a number between 8 and 128 characters");
+  var p2 = p1;
+  var upperMessage = confirm("Do you want to include upper case characters in your password?");
+  if (upperMessage){
+    p2 = p1.concat(upper);
+    console.log(p2);
+  } else {
+    p2 = p1;
+    console.log(p2);
   }
-  else (userLength)
-    return window.alert("You have chosen a password of " + userLength + " characters.");
 }
 
-passwordLength()
-
 // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-//   passwordText.value = password;
-
-
+  passwordText.value = password;
+}
 
 
 // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// getting user to select password length
-
-
+generateBtn.addEventListener("click", writePassword);
