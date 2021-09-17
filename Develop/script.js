35eb95440bf2264a679523a22357ec2b295c7732
Benjamin Clewer <benjiclewer@gmail.com>
2021-09-17 13:09:27 +0930

    struggle town, getting password length set up

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,57 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var length = Math.floor((Math.random() * 128 + 8)*Math.ceil(0.9926));
+var upper = true;
+var lower = true;
+var numbers = true;
+var specialCharacters = true;
 
-  passwordText.value = password;
+console.log(length);
 
+var passwordLength = function() {
+  var userLength = window.prompt("Select a password length between 8 and 128 characters.");
+  if (userLength <8) {
+    userLength = window.prompt("That number was less than 8, please select a number between 8 and 128 characters");
+  }
+  else if (userLength >128) {
+    userLength = window.prompt("That number was greater than 128, please select a number between 8 and 128 characters");
+  }
+  else (userLength)
+    return window.alert("You have chosen a password of " + userLength + " characters.");
 }
 
+passwordLength()
+
+// Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// getting user to select password length
+
+
